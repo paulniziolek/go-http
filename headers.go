5f8558f09b0f6472232c7de9449e5015501fc2f1
paulniziolek <niziolek.paul@gmail.com>
2025-11-07 18:23:51 +0100

    Headers method improvements

--- headers.go
+++ headers.go
@@ -1,10 +1,12 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 type Headers map[string][]string
 
-// Get returns the first value of a key, and an empty string for no values.
+// Get returns the first value of a key and an `ok` bool if it exists in the map.
 func (h Headers) Get(key string) (string, bool) {
 	if len(h[canonical(key)]) == 0 {
 		return "", false
@@ -12,16 +14,27 @@ func (h Headers) Get(key string) (string, bool) {
 	return h[canonical(key)][0], true
 }
 
+// GetAll returns all key-value pairs for the specific key.
+func (h Headers) GetAll(key string) []string {
+	return h[canonical(key)]
+}
+
 // Add adds the key-value pair to the header.
 func (h Headers) Add(key string, value string) {
 	h[canonical(key)] = append(h[canonical(key)], value)
 }
 
-// Values returns all key-value pairs for the specific key.
-func (h Headers) Values(key string) []string {
-	return h[canonical(key)]
+// ForEach iterates over all (key, value) pairs, including duplicates.
+func (h Headers) ForEach(f func(key string, value string)) {
+	for key, values := range h {
+		for _, value := range values {
+			f(key, value)
+		}
+	}
 }
 
+// canonical returns a canonical form of a key string.
+// Header keys can be case insensitive.
 func canonical(key string) string {
 	return strings.ToLower(key)
 }
