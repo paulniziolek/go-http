7f32cc26301e7ca640eeebe879c93605f58da22d
paulniziolek <niziolek.paul@gmail.com>
2025-11-14 17:56:40 +0100

    hello world http server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
+import "strconv"
+
+type helloWorldHandler struct {
+	greeting string
+}
+
+func NewHelloWorldHandler() *helloWorldHandler {
+	return &helloWorldHandler{
+		greeting: "hello there",
+	}
+}
+
+func (h *helloWorldHandler) ServeHTTP(w ResponseWriter, req *Request) {
+	if req.Method == "GET" {
+		w.Header().Set("Content-Length", strconv.Itoa(len(h.greeting)))
+		w.Write([]byte(h.greeting))
+	} else {
+		w.WriteHeader(StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	server := NewServer(":42069")
+	helloHandler := NewHelloWorldHandler()
+	server.Handle("/", helloHandler)
 	server.ListenAndServe()
 
 }
